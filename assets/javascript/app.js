5c5919978965fc508115a6e9551183fae44f96bd
yashua <yashuab.abraham@gmail.com>
2019-03-28 23:38:29 -0400

    added hover to buttons

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+  $(".name").waypoint(function(direction){
+
+      if( direction === "down"){
+        $("nav").addClass("sticky-nav")
+      } else {
+        $("nav").removeClass("sticky-nav")
+      }
+
+  })
+  // var waypoints = $('#handler-first').waypoint(function(direction) {
+  //   notify(this.element.id + ' hit 25% from top of window') 
+  // }, {
+  //   offset: '25%'
+  // })
+})
+
+
